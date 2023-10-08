36b4eadd7b800071b71be48c4908b858ceeea0ec
Bryan <bryanferrando59@gmail.com>
2023-10-08 13:31:52 +0200

    connection

--- db/connection.php
+++ db/connection.php
@@ -1,13 +1,32 @@
 <?php
 
 $host = 'localhost';
-$dbname = 'ajax_products';
+$dbname = 'bryan_ferrando_web_day10';
 $username = 'root';
 $password = '';
 
 try {
-  $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+  $pdo = new PDO("mysql:host=$host", $username, $password);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   response(['success' => false, 'error' => 'An error occured'], 500);
+}
+
+$sql = <<<SQL
+  CREATE DATABASE IF NOT EXISTS $dbname
+SQL;
+
+$pdo->query($sql);
+$pdo->query("use $dbname");
+
+function generateTable($pdo, $dbname, $table, $sql){
+  $createTableSQL = <<<SQL
+    SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ?
+  SQL;
+  
+  $result = fetchAll($pdo, $createTableSQL, [$dbname, $table]);
+  
+  if(count($result) == 0){
+    $pdo->query($sql);
+  }
 }
