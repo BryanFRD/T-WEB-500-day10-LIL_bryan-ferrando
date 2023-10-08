d9af5a4e1cb89155b2c683dabc944efe5b9d377c
Bryan <bryanferrando59@gmail.com>
2023-10-08 14:42:44 +0200

    task06

--- task06/task06.js
+++ task06/task06.js
@@ -1,4 +1,9 @@
+const refreshChatText = refreshChat.querySelector('span');
 let autoScroll = true;
+let fetchingMessages = false;
+const timeout = 100;
+const refreshRate = 4000;
+let deltaTime = 4000;
 
 const handleSubmit = (event) => {
   event.preventDefault();
@@ -6,13 +11,59 @@ const handleSubmit = (event) => {
   
   if(!name || !message)
     return;
-    
+  
   messageInput.value = '';
+  
+  fetch('./task06.php', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({username: name, message})
+  })
+  .then(response => {
+    const json = response.json().catch((err) => {
+      console.log(err);
+      ({success: false, error: 'Something went wrong'})
+    });
+    return json;
+  })
+  .then();
+}
+
+const handleRefreshButton = () => {
+  refreshChat.disabled = fetchingMessages;
+  let seconds = Math.max(0, Math.min(Math.floor((refreshRate - deltaTime) / 1000) + 1, refreshRate))
+  refreshChat.innerText = `Refreshing in ${seconds}`;
+}
+
+const fetchMessages = () => {
+  fetchingMessages = true;
+  
+  fetch('./task06.php')
+    .then(response => {
+      const json = response.json().catch((err) => {
+        console.log(err);
+        ({success: false, error: 'Something went wrong'})
+      });
+      return json;
+    })
+    .then(data => {
+      if(!data.success)
+        return;
+      
+      chatBox.innerHTML = '<div class="mt-auto"></div>';
+      data.messages?.forEach(handleMessage);
+    })
+    .finally(() => {
+      deltaTime = 0;
+      fetchingMessages = false;
+    })
 }
 
 const handleMessage = ({username, message}) => {
   const div = document.createElement('div');
-  div.classList.add('bg-secondary', 'bg-opacity-25', 'w-min', 'p-3', 'rounded-3')
+  div.classList.add('bg-secondary', 'bg-opacity-25', 'w-min', 'p-3', 'rounded-3');
   const h3 = document.createElement('h3');
   h3.innerText = username;
   const span = document.createElement('span');
@@ -24,6 +75,17 @@ const handleMessage = ({username, message}) => {
   if(autoScroll)
     chatBox.scrollTop = chatBox.scrollHeight;
 }
+
+const handleMessagesInterval = () => {
+  deltaTime += timeout;
+  handleRefreshButton();
+  if(fetchingMessages || deltaTime < refreshRate)
+    return;
+  
+  fetchMessages();
+}
+
+setInterval(handleMessagesInterval, timeout);
 
 const handleScroll = () => {
   autoScroll = chatBox.scrollTop === (chatBox.scrollHeight - chatBox.clientHeight);
@@ -37,3 +99,4 @@ const chatObserver = new ResizeObserver(() => {
 chatObserver.observe(chatBox);
 chatBox.addEventListener('scroll', handleScroll);
 form.addEventListener('submit', handleSubmit);
+refreshChat.addEventListener('click', fetchMessages);
--- task06/task06.php
+++ task06/task06.php
@@ -1,2 +1,62 @@
 <?php
 
+header('Content-Type: application/json');
+require('../db/connection.php');
+
+$messagesTableSQL = file_get_contents("../resources/messages.sql");
+generateTable($pdo, $dbname, 'messages', $messagesTableSQL);
+
+$requestMethod = strtolower($_SERVER['REQUEST_METHOD']);
+
+if(function_exists($requestMethod)){
+  $requestMethod($pdo);
+}
+
+function get($pdo){
+  $sql = <<<SQL
+    SELECT * FROM bryan_ferrando_web_day10.messages LIMIT 50
+  SQL;
+  
+  $result = fetchAll($pdo, $sql, []);
+  
+  response(['success' => true, 'messages' => $result]);
+}
+
+function post($pdo){
+  $body = json_decode(file_get_contents('php://input'), true);
+  
+  $sql = <<<SQL
+    INSERT INTO bryan_ferrando_web_day10.messages (username, message)
+    VALUES (:username, :message)
+  SQL;
+  
+  if(!isset($body['username']) || !isset($body['message'])){
+    response(['success' => false, 'message' => 'Username or message not set!'], 400);
+  }
+  
+  $result = query($pdo, $sql, $body);
+  
+  if(!$result){
+    response(['success' => false, 'message' => 'Message not saved!'], 400);
+  }
+  
+  response(['success' => true, 'message' => 'Message saved!']);
+}
+
+function fetchAll($pdo, $sql, $data){
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute($data);
+  return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function query($pdo, $sql, $data){
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute($data);
+  return ($stmt->rowCount() > 0);
+}
+
+function response($data, $code = 200){
+  http_response_code($code);
+  echo json_encode($data);
+  exit;
+}
